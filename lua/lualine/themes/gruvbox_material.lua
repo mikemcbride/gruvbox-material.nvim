0d51804b3cad785ff93656bbd650de41ce3e7e98
Mike McBride <mike.mcbride@hey.com>
2023-10-03 21:26:08 -0500

    tweak lualine again

--- lua/lualine/themes/gruvbox_material.lua
+++ lua/lualine/themes/gruvbox_material.lua
@@ -3,34 +3,34 @@ local colors = require("gruvbox_material.palette").get_base_colors("dark", "hard
 local gruvbox_material = {}
 
 gruvbox_material.normal = {
-  a = { bg = colors.blue, fg = colors.bg0 },
-  b = { bg = colors.bg1, fg = colors.blue },
-  c = { bg = colors.bg2, fg = colors.fg1 },
+  a = { bg = colors.bg2, fg = colors.bg0 },
+  b = { bg = colors.bg1, fg = colors.fg0 },
+  c = { bg = colors.bg0, fg = colors.fg1 },
 }
 
 gruvbox_material.insert = {
   a = { bg = colors.green, fg = colors.bg0 },
-  b = { bg = colors.bg1, fg = colors.green },
+  b = { bg = colors.bg0, fg = colors.green },
 }
 
 gruvbox_material.command = {
   a = { bg = colors.orange, fg = colors.bg0 },
-  b = { bg = colors.bg1, fg = colors.orange },
+  b = { bg = colors.bg0, fg = colors.orange },
 }
 
 gruvbox_material.visual = {
   a = { bg = colors.purple, fg = colors.bg0 },
-  b = { bg = colors.bg1, fg = colors.purple },
+  b = { bg = colors.bg0, fg = colors.purple },
 }
 
 gruvbox_material.replace = {
   a = { bg = colors.red, fg = colors.bg0 },
-  b = { bg = colors.bg1, fg = colors.red },
+  b = { bg = colors.bg0, fg = colors.red },
 }
 
 gruvbox_material.terminal = {
   a = {bg = colors.aqua, fg = colors.bg0 },
-  b = {bg = colors.bg1, fg=colors.aqua },
+  b = {bg = colors.bg0, fg=colors.aqua },
 }
 
 gruvbox_material.inactive = {
